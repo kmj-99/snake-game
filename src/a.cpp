7b07312db3714e2163d569bd6f21661bf9cb9cef
KimJaeYong <jykim@KimJaeYongui-MacBookAir.local>
2022-05-23 21:24:43 +0900

    [JYK] Create map

--- src/a.cpp
+++ src/a.cpp
@@ -13,51 +13,20 @@ int main()
   //palette
   start_color();
   init_pair(1, COLOR_BLUE, COLOR_YELLOW);
-  init_pair(2, COLOR_RED, COLOR_BLACK);
-  init_pair(3, COLOR_BLUE, COLOR_WHITE);
-  bkgd(COLOR_PAIR(3));
+  init_pair(2, COLOR_RED, COLOR_BLACK); // background color
+  init_pair(3, COLOR_BLACK, COLOR_WHITE);
+  // bkgd(COLOR_PAIR(2));
 
-  WINDOW * main_window = newwin(40, 120, 0, 0);
+  WINDOW * main_window;
+  main_window = newwin(40, 125, 0, 0);
+  wborder(main_window, '|', '|', '-', '-', '*', '*', '*', '*');
   wbkgd(main_window, COLOR_PAIR(3));
 
-  refresh();
 
+  refresh();
+  wrefresh(main_window);
   Map init;
 
-  // setlocale(LC_ALL, "");
-  // Curses 모드 시작 start_color(); // Color 사용 선언 init_pair(1, COLOR_RED,COLOR_WHITE);
-
-
-  //map size (22,22)
-
-
-
-
-  //background color
-
-
-  // mvprintw(1, 1, "Hello world");
-
-  // attron(COLOR_PAIR(1));
-  // border('|', '|', '-', '-', '*', '*', '*', '*');
-  // mvprintw(5, 5, "Hello world");
-  // attroff(COLOR_PAIR(1));
-
-  //border
-
-
-  // attron(COLOR_PAIR(2));
-  // for (size_t i = 0; i < 60; i++)
-  // {
-  //   mvprintw(5, i, " ");
-  // }
-  // for (size_t i = 5; i < 65; i++)
-  // {
-  //   mvprintw(i, 0, " ");
-  // }
-  // attroff(COLOR_PAIR(2));
-
-  //refresh
 
   getch();
   endwin();

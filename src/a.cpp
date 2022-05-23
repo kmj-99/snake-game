3f0df9d93bffe8ee112090342a35eae6a5b1d7a9
kmj-99 <nex1223@naver.com>
2022-05-23 22:58:04 +0900

    [KMJ] Finished Keyboard Input

--- src/a.cpp
+++ src/a.cpp
@@ -3,8 +3,14 @@
 #include <clocale>
 #include "map.h"
 
+#define UP 259
+#define RIGHT 261
+#define LEFT 260
+#define DOWN 258
+
 using namespace std;
 
+void move();
 
 int main()
 {
@@ -28,7 +34,46 @@ int main()
   Map init;
 
 
+
+
+while(1){
+  move();
+
+  refresh();
+}
+
+
+
   getch();
   endwin();
   return 0;
+}
+
+
+
+void move(){
+  keypad(stdscr,TRUE); // 입력 시 키보드 특수 키 입력을 가능하게 설정하는 함수 (ex- ESC,F!,F@,방향키..)
+  curs_set(0); // 화면에 보이는 커서 설정 0 - 커서 사라짐 , 1- 일반커서 , 2- 큰 커서
+  noecho(); // 입력한 값을 화면에 보이지 않게함
+
+	int move = getch();
+	switch(move)
+	{
+		case UP:
+      printw("UP");
+			break;
+		case RIGHT:
+      printw("RIGHT");
+			break;
+		case LEFT:
+      printw("LEFT");
+			break;
+		case DOWN:
+      printw("DOWN");
+			break;
+		case KEY_BACKSPACE:
+      printw("oh ho");
+			break;
+	}
+
 }

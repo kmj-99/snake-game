0c7570a85eb2acc0dd48be03fc459ae4020debd3
KimJaeYong <jykim@KimJaeYongui-MacBookAir.local>
2022-05-29 17:35:59 +0900

    [JYK] Implement snake movement and map rendering

--- src/map.h
+++ src/map.h
@@ -1,15 +1,20 @@
 #include <unistd.h>
 #include <signal.h>
+#include <vector>
 
 using namespace std;
 
 class Map{
     public:
+        vector<vector<int> > snake; // snake ((head x, head y),(body x, body y),(body)...)
         WINDOW *main_board, *score_board, *mission_board;
+          
         // int start_point1[2] = {5, 10};
         // int start_point2[2] = {5, 73};
         // int start_point3[2] = {23, 73};
         Map(){
+            vector<int> head{15, 30};
+            snake.push_back(head);
             start_color();            
             main_board = newwin(30, 60, 5, 10);
             wbkgd(main_board, COLOR_PAIR(2));
@@ -31,17 +36,6 @@ class Map{
             wborder(mission_board, '*', '*', '*', '*', '*', '*', '*', '*');
             mvwprintw(mission_board, 0, 14, "Mission Board");
 
-            render_map();
-            int i = 5;
-            while(true){
-                alarm(1);
-                pause();
-                render_map();
-                a[1][i++] = 3;
-                cout << i << endl;
-                if(i == 55)
-                    break;
-            }
         };
 
         int a[30][60] = {
@@ -78,31 +72,5 @@ class Map{
                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                    {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
         };
-
-
-    void render_map(){
-        for (size_t i = 0; i < 30; i++)
-            for (size_t j = 0; j < 60; j++)
-            {
-                if (this -> a[i][j] == 0)
-                {
-                    wattron(main_board, COLOR_PAIR(2));
-                    mvwprintw(main_board, i, j, " ");
-                    wattroff(main_board, COLOR_PAIR(2));
-                }
-                else if(this -> a[i][j] == 1){
-                    wattron(main_board, COLOR_PAIR(1));
-                    mvwprintw(main_board, i, j, " ");
-                    wattroff(main_board, COLOR_PAIR(1));
-                }
-                else if(this -> a[i][j] == 3){
-                    wattron(main_board, COLOR_PAIR(4));
-                    mvwprintw(main_board, i, j, "M");
-                    wattroff(main_board, COLOR_PAIR(4));                        
-                }
-            }
-        wrefresh(main_board);
-        wrefresh(score_board);
-        wrefresh(mission_board);
-    };
+        
 };

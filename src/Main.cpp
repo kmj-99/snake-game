27becd97709256c509bda6a6756e371a37058797
KimJaeYong <jykim@KimJaeYongui-MacBookAir.local>
2022-05-29 17:38:09 +0900

    [JYK] Seperate .h file

---
+++ src/Main.cpp
@@ -0,0 +1,63 @@
+#include <ncurses.h>
+#include <iostream>
+#include <clocale>
+#include <unistd.h>
+#include "GameController.h"
+using namespace std;
+
+int main()
+{
+  //alarm handler
+  // signal(SIGALRM, sig_alrm);
+
+  //init screen
+  try
+  {
+    initscr();
+  }
+  catch(const std::exception& e)
+  {
+    cout << "cant start game!!" << endl;
+    std::cerr << e.what() << '\n';
+  }
+
+  //keypad setting
+  keypad(stdscr, TRUE);
+  
+  //cursor setting
+  curs_set(0);
+  noecho();
+
+  //refresh background
+  resize_term(200, 200);
+  refresh();
+  
+  //palette
+  start_color();
+  init_pair(1, COLOR_BLUE, COLOR_YELLOW); // wall color
+  init_pair(2, COLOR_RED, COLOR_BLACK); // game screen color
+  init_pair(3, COLOR_BLACK, COLOR_WHITE); // background color
+  init_pair(4, COLOR_MAGENTA, COLOR_BLACK); // snake color
+
+  // background window
+  WINDOW *main_window;
+  nodelay(stdscr, true);
+  main_window = newwin(40, 125, 0, 0);
+  wborder(main_window, '|', '|', '-', '-', '*', '*', '*', '*');
+  wbkgd(main_window, COLOR_PAIR(3));
+  wrefresh(main_window);
+  
+  // game setting
+  GameController start;
+  
+  getch();
+  endwin();
+  return 0;
+}
+
+
+
+
+
+
+
---
+++ src/Snake.h
@@ -0,0 +1,40 @@
+#include <vector>
+
+using namespace std;
+
+class Snake{
+    public:
+        //snake vector ((head x, head y),(body x, body y),(body)(body)...)
+        vector<vector<int> > body; // body just follow head
+        int size; // size of snake, default = 1
+
+        //Constructor
+        Snake(int size = 1) : size(size){
+            vector<int> head{15, 30};
+            body.push_back(head);
+        };
+
+        //size up 
+        void size_up(){};
+
+        //move snake
+        void move(int command){
+        switch(command)
+        {
+            case 1: // up
+                body[0][0]--;  
+                break;
+            case 2: // right
+                body[0][1]++;
+                break;
+            case 3: // left
+                body[0][1]--;
+                break;
+            case 4: // down
+                body[0][0]++;
+                break;
+            case 5: // backspace
+                break;
+        }
+    }
+};
